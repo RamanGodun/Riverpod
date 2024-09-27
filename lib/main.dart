097458c91b5ae4dce85679b8bdd2d3ca1e949baf
Roman Godun <4l.roman.godun@gmail.com>
2024-09-27 17:17:33 +0300

    chore

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(
+    child: MainApp(),
+  ));
 }
 
 class MainApp extends StatelessWidget {
@@ -9,10 +12,19 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: 'Riverpod study',
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: ListView(
+            shrinkWrap: true,
+            children: const [
+              Text(
+                'Hello World TOp!',
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
         ),
       ),
     );
