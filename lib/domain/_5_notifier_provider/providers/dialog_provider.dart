180cdf8d9c7764c8f9ae14e9ecc1c810440fc8c8
Roman Godun <4l.roman.godun@gmail.com>
2024-10-03 16:18:19 +0300

    fix: fixed typo

--- lib/domain/_5_notifier_provider/providers/dialog_provider.dart
+++ lib/domain/_5_notifier_provider/providers/dialog_provider.dart
@@ -3,7 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../data/models/enum_activity_state.dart';
 import '../../../widgets/error_dialog.dart';
-import 'enum_activity_provider.dart';
+import 'enum_shape_state/enum_activity_provider.dart';
 
 final errorHandlingProvider = Provider.autoDispose<ErrorHandlingService>((ref) {
   return ErrorHandlingService();
